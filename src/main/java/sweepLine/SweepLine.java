c8a0884f211810f4b446760909dafd96e95875ad
st116552 <st116552@student.spbu.ru>
2024-08-08 18:48:45 +0300

    corrections SweepLine

--- src/main/java/sweepLine/SweepLine.java
+++ src/main/java/sweepLine/SweepLine.java
@@ -5,97 +5,67 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class SweepLine {
 	double inaccuracy;
+
 	public SweepLine(double inaccuracy) {
 		this.inaccuracy = inaccuracy;
 	}
+
 	public void makePlanar(Graph gph) {
 		EdgeOfGraph[] edgesList = gph.edgesArray();
-		HashMap<Integer, ArrayList<EdgeOfGraph>> intersectionPoints = findPointsOfIntersection(edgesList);
+		ArrayList<ArrayList<Vertex>> intersectionPoints = findPointsOfIntersection(edgesList);
 		addIntersectionPoints(gph, edgesList, intersectionPoints);
-		
+
 	}
+
 	private void addIntersectionPoints(Graph gph, EdgeOfGraph[] edgesList,
-			HashMap<Integer, ArrayList<EdgeOfGraph>> intersectionPoints) {
-		HashMap<EdgeOfGraph, ArrayList<Vertex>> edgesVertices = new HashMap<EdgeOfGraph, ArrayList<Vertex>>();
+			ArrayList<ArrayList<Vertex>> intersectionPoints) {
 		for (int i = 0; i < edgesList.length; i++) {
-			edgesVertices.put(edgesList[i], new ArrayList<Vertex>());
-			edgesVertices.get(edgesList[i]).add(edgesList[i].getBegin());
-			edgesVertices.get(edgesList[i]).add(edgesList[i].getEnd());
-		}
-		for (int i = 0; i < edgesList.length; i++) {
-			if (intersectionPoints.get(i) == null || (intersectionPoints.get(i) != null && intersectionPoints.get(i).size() == 0)) {
+			if (intersectionPoints.get(i) == null
+					|| (intersectionPoints.get(i) != null && intersectionPoints.get(i).size() == 0)) {
 				continue;
 			}
-			for (int j = 0; j < intersectionPoints.get(i).size(); j++) {
-				Vertex intersectionPoint = edgesList[i].intersectionPoint(intersectionPoints.get(i).get(j));
-				if (intersectionPoint.getPoint().getX() == -1 || intersectionPoint.getPoint().getY() == -1) continue;
-				Vertex begin1 = maxBegin(intersectionPoint.getPoint().getX(), edgesVertices.get(edgesList[i]));
-				Vertex begin2 = maxBegin(edgesList[i].getBegin().getPoint().getX() < edgesList[i].getEnd().getPoint().getX() ? edgesList[i].getBegin().getPoint().getX() : edgesList[i].getEnd().getPoint().getX(),
-						edgesVertices.get(intersectionPoints.get(i).get(j)));
-				Vertex end1 = minEnd(intersectionPoint.getPoint().getX(), edgesVertices.get(edgesList[i]));
-				Vertex end2 = minEnd(edgesList[i].getBegin().getPoint().getX() < edgesList[i].getEnd().getPoint().getX() ? edgesList[i].getBegin().getPoint().getX() : edgesList[i].getEnd().getPoint().getX(),
-						edgesVertices.get(intersectionPoints.get(i).get(j)));
+			gph.deleteEdge(edgesList[i].getBegin(), edgesList[i].getEnd());
+			if (edgesList[i].getBegin().getPoint().getX() > edgesList[i].getEnd().getPoint().getX()) {
+				gph.addEdge(edgesList[i].getBegin(), intersectionPoints.get(i).get(intersectionPoints.get(i).size() - 1),
+						edgesList[i].getBegin().getLength(intersectionPoints.get(i).get(intersectionPoints.get(i).size() - 1)));
+				gph.addEdge(intersectionPoints.get(i).get(0), edgesList[i].getEnd(),
+						edgesList[i].getEnd()
+								.getLength(intersectionPoints.get(i).get(0)));
+				for (int j = 1; j < intersectionPoints.get(i).size(); j++) {
+					gph.addEdge(intersectionPoints.get(i).get(j), intersectionPoints.get(i).get(j - 1),
+							intersectionPoints.get(i).get(j - 1).getLength(intersectionPoints.get(i).get(j)));
+				}
+			} else {
+				gph.addEdge(edgesList[i].getBegin(), intersectionPoints.get(i).get(0),
+						edgesList[i].getBegin().getLength(intersectionPoints.get(i).get(0)));
+				gph.addEdge(intersectionPoints.get(i).get(intersectionPoints.get(i).size() - 1), edgesList[i].getEnd(),
+						edgesList[i].getEnd()
+								.getLength(intersectionPoints.get(i).get(intersectionPoints.get(i).size() - 1)));
+				for (int j = 1; j < intersectionPoints.get(i).size(); j++) {
+					gph.addEdge(intersectionPoints.get(i).get(j - 1), intersectionPoints.get(i).get(j),
+							intersectionPoints.get(i).get(j - 1).getLength(intersectionPoints.get(i).get(j)));
+				}
+			}
+		}
 
-				edgesVertices.get(edgesList[i]).add(intersectionPoint);
-				edgesVertices.get(intersectionPoints.get(i).get(j)).add(intersectionPoint);
-				gph.addVertex(intersectionPoint);
-				if (edgesList[i].getBegin().getPoint().getX() > edgesList[i].getEnd().getPoint().getX()) {
-					Vertex tmp = begin1;
-					begin1 = end1;
-					end1 = tmp;
-				}
-				if (intersectionPoints.get(i).get(j).getBegin().getPoint().getX() > intersectionPoints.get(i).get(j).getEnd().getPoint().getX()) {
-					Vertex tmp = begin2;
-					begin2 = end2;
-					end2 = tmp;
-				}
-				gph.deleteEdge(begin1, end1);
-				gph.deleteEdge(begin2, end2);
-				gph.addEdge(begin1, intersectionPoint, begin1.getLength(intersectionPoint));
-				gph.addEdge(begin2, intersectionPoint, begin2.getLength(intersectionPoint));
-				gph.addEdge(intersectionPoint, end1, end1.getLength(intersectionPoint));
-				gph.addEdge(intersectionPoint, end2, end2.getLength(intersectionPoint));
-			}
-		}
-		
 	}
-	private Vertex minEnd(double x, ArrayList<Vertex> arrayList) {
-		if (arrayList.isEmpty()) return null;
-		Vertex ans = new Vertex(0, new Point(-1, -1));
-		for (int i = 0; i < arrayList.size(); i++) {
-			if (x < arrayList.get(i).getPoint().getX() && ans.getPoint().getX() == -1) {
-				ans = arrayList.get(i);
-			}
-			if ( ans.getPoint().getX() != -1 && x < arrayList.get(i).getPoint().getX() && arrayList.get(i).getPoint().getX() < ans.getPoint().getX()) {
-				ans = arrayList.get(i);
-			}	
+
+	public ArrayList<ArrayList<Vertex>> findPointsOfIntersection(EdgeOfGraph[] edgesList) {
+		ArrayList<ArrayList<Vertex>> intersectionPoints = new ArrayList<ArrayList<Vertex>>();
+		for (int i = 0; i < edgesList.length; i++) {
+			intersectionPoints.add(new ArrayList<Vertex>());
 		}
-		return ans;
-	}
-	private Vertex maxBegin(double x, ArrayList<Vertex> arrayList) {
-		if (arrayList.isEmpty()) return null;
-		Vertex ans = new Vertex(0, new Point(-1, -1));
-		for (int i = 0; i < arrayList.size(); i++) {
-			if (x > arrayList.get(i).getPoint().getX() && arrayList.get(i).getPoint().getX() > ans.getPoint().getX()) {
-				ans = arrayList.get(i);
-			}	
-		}
-		return ans;
-	}
-	public HashMap<Integer, ArrayList<EdgeOfGraph>> findPointsOfIntersection(EdgeOfGraph[] edgesList) {
-		HashMap<Integer, ArrayList<EdgeOfGraph>> intersectionPoints = new HashMap <Integer, ArrayList<EdgeOfGraph>>();
 		ArrayList<Action> actions = initActions(edgesList);
 		Collections.sort(actions, new Comparator<Action>() {
 			@Override
 			public int compare(Action a1, Action a2) {
-				return a1.getX() < a2.getX() ? -1 : a1.getX() > a2.getX() ? 1 : 0;
+				return a1.getX() < a2.getX() ? -1 : a1.getX() > a2.getX() ? 1 : a1.getType() == ActionType.ADD ? 1 : a2.getType() == ActionType.ADD ? -1 : 0;
 			}
 		});
-		HashSet<EdgeOfGraph> actualEdge = new HashSet<EdgeOfGraph>();
+		HashMap<Integer, EdgeOfGraph> actualEdge = new HashMap<Integer, EdgeOfGraph>();
 //		TreeSet<EdgeOfGraph> actualEdge = new TreeSet<EdgeOfGraph>(new Comparator<EdgeOfGraph>() {
 //			@Override
 //			public int compare(EdgeOfGraph o1, EdgeOfGraph o2) {
@@ -106,15 +76,17 @@ public class SweepLine {
 //		});
 		for (int i = 0; i < actions.size(); i++) {
 			if (actions.get(i).getType() == ActionType.ADD) {
-				intersectionPoints.put(actions.get(i).getEdgeNum(), new ArrayList<EdgeOfGraph>());
-				for (EdgeOfGraph edge : actualEdge) {				
-					if (edgesList[actions.get(i).getEdgeNum()].intersect(edge)) {
-						intersectionPoints.get(actions.get(i).getEdgeNum()).add(edge);
+				for (int edgeNum : actualEdge.keySet()) {
+					if (edgesList[actions.get(i).getEdgeNum()].intersect(actualEdge.get(edgeNum))) {
+						Vertex intersecPoint = edgesList[actions.get(i).getEdgeNum()]
+								.intersectionPoint(actualEdge.get(edgeNum));
+						intersectionPoints.get(actions.get(i).getEdgeNum()).add(intersecPoint);
+						intersectionPoints.get(edgeNum).add(intersecPoint);
 					}
 				}
-				actualEdge.add(edgesList[actions.get(i).getEdgeNum()]);
+				actualEdge.put(actions.get(i).getEdgeNum(), edgesList[actions.get(i).getEdgeNum()]);
 			} else {
-				actualEdge.remove(edgesList[actions.get(i).getEdgeNum()]);
+				actualEdge.remove(actions.get(i).getEdgeNum());
 //				for (EdgeOfGraph edge : actualEdge) {				
 //					if (edgesList[actions.get(i).getEdgeNum()].intersect(edge)) {
 //						intersectionPoints.get(actions.get(i).getEdgeNum()).add(edge);
@@ -124,15 +96,16 @@ public class SweepLine {
 		}
 		return intersectionPoints;
 	}
-	
 
-
-	
 	private ArrayList<Action> initActions(EdgeOfGraph[] edgesList) {
 		ArrayList<Action> result = new ArrayList<Action>();
 		for (int i = 0; i < edgesList.length; i++) {
-			result.add(new Action(Math.min(edgesList[i].getBegin().getPoint().getX(),edgesList[i].getEnd().getPoint().getX()), i, ActionType.ADD));
-			result.add(new Action(Math.max(edgesList[i].getBegin().getPoint().getX(),edgesList[i].getEnd().getPoint().getX()), i, ActionType.DELETE));
+			result.add(new Action(
+					Math.min(edgesList[i].getBegin().getPoint().getX(), edgesList[i].getEnd().getPoint().getX()), i,
+					ActionType.ADD));
+			result.add(new Action(
+					Math.max(edgesList[i].getBegin().getPoint().getX(), edgesList[i].getEnd().getPoint().getX()), i,
+					ActionType.DELETE));
 		}
 		return result;
 	}
